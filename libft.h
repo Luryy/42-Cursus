4284c80a9687aafbacb57497c42ffe4f05bdc153
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-05-18 19:47:32 -0300

    create libft.h

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/18 18:45:00 by lyuri-go          #+#    #+#             */
+/*   Updated: 2021/05/18 19:45:27 by lyuri-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+int		ft_strlen(char *str);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(char *src);
+
+#endif
