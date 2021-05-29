888f18f23c632e4b0818f3a2f9a86cefbd5d593d
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-05-29 17:27:38 -0300

    fix c repetition

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,33 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 16:44:00 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/05/27 18:33:33 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/05/29 17:25:02 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_times(char const *s, char c)
+static int	*ft_count_times(char const *s, char c)
 {
 	int i;
-	int c_times;
+	int is_sequence;
+	int *c_times;
 
 	i = -1;
+	c_times = malloc(3 * sizeof(int));
+	is_sequence = 1;
 	while(s[++i] != 0)
+	{
 		if (s[i] == c)
-			c_times++;
+		{
+			c_times[0]++;
+			if (!is_sequence)
+				c_times[1]++;
+			is_sequence = 1;
+		}
+		else
+			is_sequence = 0;
+	}
 	return (c_times);
 }
 
@@ -49,22 +61,26 @@ char	**ft_split(char const *s, char c)
 	char	**split;
 	int		i;
 	int		*j;
-	int		c_times;
+	int		*c_times;
+	int		k;
 	int		offset;
 
 	if (!s)
 		return (NULL);
 	c_times = ft_count_times(s, c);
-	j = malloc((c_times + 2) * sizeof(int));
+	j = malloc((c_times[0] + 2) * sizeof(int));
 	ft_count_word_length(j, s, c);
-	split = malloc((c_times + 2) * sizeof(char *));
+	split = malloc((c_times[1] + 2) * sizeof(char *));
 	i = -1;
+	k = 0;
 	offset = 0;
-	while (++i < c_times + 2)
+
+	while (++i < c_times[0] + 2)
 	{
-		split[i] = ft_substr(s, offset, (size_t)j[i]);
+		if (j[i] != 0)
+			split[k++] = ft_substr(s, offset, (size_t)j[i]);
 		offset += j[i] + 1;
 	}
-	split[i] = '\0';
+	split[k] = '\0';
 	return (split);
 };
