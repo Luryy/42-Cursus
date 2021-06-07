8c5385f935c68d35bac9e7115a62b3e4418b4e73
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-06-07 18:16:44 -0300

    fix not allowed functions at putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 15:44:01 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/06/01 16:44:45 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/06/07 18:14:47 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*char_int;
+	long int	nbr;
 
-	char_int = ft_itoa(n);
-	ft_putstr_fd(char_int, fd);
+	nbr = n;
+	if (nbr < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr *= -1;
+	}
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd('0' + (nbr % 10), fd);
 }
