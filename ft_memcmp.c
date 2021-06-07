6ed188fcdf3ccd21c2dcc505e75acac0d3abcce3
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-06-07 17:24:45 -0300

    fix null verification at memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 11:41:24 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/06/01 16:43:43 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/06/07 17:24:00 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	char_s1 = s1;
 	char_s2 = s2;
-	while ((char_s1[i] != '\0' || char_s2[i] != '\0') && (i < (unsigned int)n))
+	while (i < (unsigned int)n)
 	{
 		r = char_s1[i] - char_s2[i];
 		if (r != 0)
