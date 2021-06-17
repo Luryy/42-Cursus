b81c10370760422e24259eadb30223136e8f9d0f
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-06-17 08:41:27 -0300

    feat: commit resolution

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,81 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 08:29:09 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/06/14 08:49:07 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/06/17 08:39:52 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*ft_join_and_free(char *to_free, char *to_join)
+{
+	int		len1;
+	int		len2;
+	int		i;
+	char	*dst;
+
+	if (!to_free)
+		return (ft_strdup(to_join));
+	if (!to_join)
+		return (NULL);
+	len1 = ft_strlen(to_free);
+	len2 = ft_strlen(to_join);
+	dst = (char *) malloc((len1 + len2 + 1) * sizeof(char));
+	if (!dst)
+		return (NULL);
+	i = -1;
+	while (to_free[++i])
+		dst[i] = to_free[i];
+	free(to_free);
+	to_free = NULL;
+	i = -1;
+	while (to_join[++i])
+		dst[len1 + i] = to_join[i];
+	dst[len1 + i] = '\0';
+	return (dst);
+}
+
+int	get_line(char **str, char **line, int i)
+{
+	int		len;
+
+	*line = ft_substr(*str, 0, i);
+	i++;
+	len = ft_strlen(*str + i) + 1;
+	ft_memmove(*str, *str + i, len);
+	if (*str && *str[0] == '\0')
+	{
+		free(*str);
+		*str = NULL;
+	}
+	return (1);
+}
+
 int	get_next_line(int fd, char **line)
 {
-	return (0);
+	static char		*str;
+	char			read_buffer[BUFFER_SIZE + 1];
+	int				nr_bytes;
+
+	if (fd < 0 || !line || BUFFER_SIZE < 1 || fd > MAX_FILE_DESCRIPTOR)
+		return (-1);
+	if (str && ft_strchr(str, '\n'))
+		return (get_line(&str, line, ft_strchr(str, '\n') - str));
+	nr_bytes = read(fd, read_buffer, BUFFER_SIZE);
+	while (nr_bytes > 0)
+	{
+		read_buffer[nr_bytes] = '\0';
+		str = ft_join_and_free(str, read_buffer);
+		if (ft_strchr(str, '\n'))
+			return (get_line(&str, line, ft_strchr(str, '\n') - str));
+		nr_bytes = read(fd, read_buffer, BUFFER_SIZE);
+	}
+	*line = ft_strdup("");
+	if (str)
+	{
+		*line = ft_strdup(str);
+		free(str);
+		str = NULL;
+	}
+	return (nr_bytes);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,24 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 08:29:32 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/06/14 08:38:23 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/06/16 21:58:06 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-int	get_next_line(int fd, char **line);
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# define MAX_FILE_DESCRIPTOR FOPEN_MAX
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(char *source);
+int		get_next_line(int fd, char **line);
+char	*ft_strchr(char *s, int c);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
