fffb87c85a22a796d900e3249194c2d96c4fcbb8
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-09-14 19:29:05 -0300

    feat: add read file at fdf

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,12 +6,15 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 08:45:17 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/09/01 08:44:09 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/09/14 19:11:57 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+# include <stdio.h>
+# include <fcntl.h>
 
 # include "../libs/libft/libft.h"
 # include "../libs/minilibx/mlx.h"
