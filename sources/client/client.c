eb6cca4828926a535401322ebb57c1435c3bc623
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-10-05 19:07:19 -0300

    feat: create client and server

--- sources/client/client.c
+++ sources/client/client.c
@@ -1,19 +1,61 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 08:36:42 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/09/27 08:49:17 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/10/05 18:52:55 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
-int	main(void)
+void	handle_client(int sig)
 {
-	ft_printf("oi");
+	if (sig == SIGUSR1)
+	{
+		ft_printf("Finished signals successfully\n");
+		exit(0);
+	}
+}
+
+void	send_char(int pid, unsigned char c)
+{
+	int	count;
+
+	count = 0;
+	while (count < 8)
+	{
+		if (c % 2 == 0 || c == 0)
+		{
+			kill(pid, SIGUSR1);
+			usleep(1100);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+			usleep(1100);
+		}
+		c /= 2;
+		count++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	server_pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Wrong input. Should be SERVER PID + MESSAGE\n");
+		return (1);
+	}
+	server_pid = ft_atoi(argv[1]);
+	while (*argv[2])
+		send_char(server_pid, *argv[2]++);
+	signal(SIGUSR1, handle_client);
+	send_char(server_pid, '\0');
 	return (0);
 }
--- sources/server/server.c
+++ sources/server/server.c
@@ -1,19 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 08:36:42 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/09/27 08:49:17 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/10/05 18:51:38 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
+void	ft_sigaction(int sig, siginfo_t *siginfo, void *context)
+{
+	static unsigned int	count;
+	static unsigned int	letter;
+
+	(void)context;
+	if (count > 7)
+	{
+		count = 0;
+		letter = 0;
+	}
+	if (sig == SIGUSR2)
+	{
+		letter += 1 << count;
+	}
+	count++;
+	if (count == 8)
+	{
+		if (letter == 0)
+		{
+			write(1, "\n", 1);
+			kill(siginfo->si_pid, SIGUSR1);
+		}
+		else
+			write(1, &letter, 1);
+	}
+}
+
 int	main(void)
 {
-	ft_printf("oi");
-	return (0);
+	struct sigaction	sig;
+
+	sig.sa_flags = SA_SIGINFO;
+	sig.sa_sigaction = ft_sigaction;
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
+	ft_printf("[SERVER] > listening on pid %d\n", getpid());
+	while (1)
+	{
+		pause();
+	}
 }
