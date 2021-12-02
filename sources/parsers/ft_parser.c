a271adbda89aa3bebba9996de75eff42f5f75d9f
Lucas Yuri <lyuri-go@student.42sp.org.br>
2021-12-02 22:36:07 +0100

    refactor: set more describable names

--- sources/parsers/ft_parser.c
+++ sources/parsers/ft_parser.c
@@ -6,50 +6,50 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 19:12:12 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/12/01 00:10:19 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/12/02 22:35:42 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static int	ft_parser_args(char *line, int i, t_exec *exec, int k)
+static int	ft_parser_args(char *line, int init_arg, t_exec *exec, int arg_nb)
 {
 	int	end_arg;
 
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	end_arg = i;
+	while (line[init_arg] == ' ' || line[init_arg] == '\t')
+		init_arg++;
+	end_arg = init_arg;
 	while (line[end_arg] && line[end_arg] != ' ' && line[end_arg] != '\t')
 		end_arg++;
-	if (end_arg != i)
-		exec->args[k] = ft_substr(line, i, end_arg - i);
+	if (end_arg != init_arg)
+		exec->args[arg_nb] = ft_substr(line, init_arg, end_arg - init_arg);
 	else
-		exec->args[k] = '\0';
+		exec->args[arg_nb] = '\0';
 	return (end_arg);
 }
 
 int	ft_parser_exec(char *line, t_exec *exec)
 {
-	int		i;
-	int		j;
-	int		k;
+	int		str_init;
+	int		str_end;
+	int		args_counter;
 
 	exec->next_type = LAST;
 	exec->args = malloc(40 * sizeof(char *));
-	i = 0;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	j = i;
-	while (line[j] && line[j] != ' ' && line[j] != '\t')
-		j++;
-	exec->cmd = ft_substr(line, i, j - i);
-	k = 0;
-	while (line[j])
+	str_init = 0;
+	while (line[str_init] == ' ' || line[str_init] == '\t')
+		str_init++;
+	str_end = str_init;
+	while (line[str_end] && line[str_end] != ' ' && line[str_end] != '\t')
+		str_end++;
+	exec->cmd = ft_substr(line, str_init, str_end - str_init);
+	args_counter = 0;
+	while (line[str_end])
 	{
-		j = ft_parser_args(line, j, exec, k);
-		k++;
+		str_end = ft_parser_args(line, str_end, exec, args_counter);
+		args_counter++;
 	}
-	exec->args[k] = '\0';
+	exec->args[args_counter] = '\0';
 	return (exec->next_type);
 }
 
