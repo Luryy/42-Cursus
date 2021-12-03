5ffb9683dfe195c9b998dc40587c69bd732028bd
Lucas Yuri <lucasyuri56@hotmail.com>
2021-12-03 17:40:27 -0300

    feat: add redirect parser

--- sources/parsers/ft_parser.c
+++ sources/parsers/ft_parser.c
@@ -6,7 +6,7 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 19:12:12 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/12/02 22:50:19 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/12/03 17:39:47 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,49 +28,56 @@ static int	ft_parser_args(char *line, int init_arg, t_exec *exec, int arg_nb)
 	return (end_arg);
 }
 
-int	ft_parser_exec(char *line, t_exec *exec)
+int	ft_parser_exec(char *line, t_exec *exec, int *str_init)
 {
-	int		str_init;
-	int		str_end;
-	int		args_counter;
+	int	str_end;
+	int	args_counter;
 
 	exec->next_type = LAST;
 	exec->args = malloc(40 * sizeof(char *));
-	str_init = 0;
-	while (line[str_init] == ' ' || line[str_init] == '\t')
-		str_init++;
-	str_end = str_init;
+	while (line[*str_init] == ' ' || line[*str_init] == '\t')
+		(*str_init)++;
+	str_end = *str_init;
 	while (line[str_end] && line[str_end] != ' ' && line[str_end] != '\t')
 		str_end++;
-	exec->cmd = ft_substr(line, str_init, str_end - str_init);
+	exec->cmd = ft_substr(line, *str_init, str_end - *str_init);
 	args_counter = 0;
-	while (line[str_end])
+	while (line[str_end] && exec->next_type == LAST)
 	{
 		str_end = ft_parser_args(line, str_end, exec, args_counter);
 		args_counter++;
 	}
 	exec->args[args_counter] = '\0';
+	*str_init = str_end;
 	return (exec->next_type);
 }
 
 void	ft_free_parser(t_exec *exec)
 {
 	int	i;
+	int	j;
 
 	i = -1;
-	if (exec->args)
-		while (exec->args[++i])
-			free(exec->args[i]);
-	free(exec->cmd);
-	free(exec->args);
+	while (exec[++i].cmd)
+	{
+		j = -1;
+		if (exec[i].args)
+			while (exec[i].args[++j])
+				free(exec[i].args[j]);
+		free(exec[i].cmd);
+		free(exec[i].args);
+	}
 	free(exec);
 }
 
 void	ft_parser(char *line, t_exec *exec_info)
 {
 	int	i;
+	int	starter;
 
 	i = 0;
-	while (ft_parser_exec(line, &exec_info[i]))
+	starter = 0;
+	while (ft_parser_exec(line, &(exec_info[i]), &starter))
 		i++;
+	exec_info[i + 1].cmd = '\0';
 }
--- sources/parsers/ft_special.c
+++ sources/parsers/ft_special.c
@@ -6,7 +6,7 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 22:28:52 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/12/02 22:56:00 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2021/12/03 17:37:57 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,35 @@ static void	ft_parse_single_quotes(char *line, int *init_arg)
 	}
 }
 
+static void	ft_parse_redirect(char *line, int *init_arg, t_exec *exec)
+{
+	if (line[*init_arg] == '|')
+		exec->next_type = PIPE;
+	else if (line[*init_arg] == '>')
+	{
+		if (line[*init_arg + 1] && line[*init_arg + 1] == '>' && (*init_arg)++)
+			exec->next_type = REDIRECT_TO_DOUBLE;
+		else
+			exec->next_type = REDIRECT_TO_SINGLE;
+	}
+	else if (line[*init_arg] == '<')
+	{
+		if (line[*init_arg + 1] && line[*init_arg + 1] == '<' && (*init_arg)++)
+			exec->next_type = REDIRECT_FROM_DOUBLE;
+		else
+			exec->next_type = REDIRECT_FROM_SINGLE;
+	}
+}
+
 void	ft_parse_special(char *line, int *init_arg, t_exec *exec)
 {
-	(void)exec;
+	int	comparer;
+
+	comparer = *init_arg;
 	ft_parse_double_quotes(line, init_arg);
-	ft_parse_single_quotes(line, init_arg);
+	if (comparer == *init_arg)
+		ft_parse_single_quotes(line, init_arg);
+	if (comparer == *init_arg)
+		ft_parse_redirect(line, init_arg, exec);
 	(*init_arg)++;
 }
