8562fba18efc86105751ee0b823fc20e0a52477f
Eduardo <eduardomendes.dev@gmail.com>
2021-12-12 15:00:05 -0300

    fix function get_env, that was returning wrong value and clean code

--- sources/utils/ft_get_env.c
+++ sources/utils/ft_get_env.c
@@ -6,7 +6,7 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 20:40:25 by lyuri-go          #+#    #+#             */
-/*   Updated: 2021/12/10 21:08:13 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/12/12 14:51:35 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_get_env(char *env)
 	envs = (mini_s()->envs);
 	while (envs->next != NULL)
 	{
-		if (ft_strncmp(env, envs->key, ft_strlen(env)))
-			return (envs->key);
-		envs = envs->next; 
+		if (!ft_strncmp(env, envs->key, ft_strlen(env)))
+			return (envs->value);
+		envs = envs->next;
 	}
 	return (NULL);
 }
--- sources/utils/ft_node.c
+++ sources/utils/ft_node.c
@@ -6,28 +6,11 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 19:50:37 by elima-me          #+#    #+#             */
-/*   Updated: 2021/12/10 22:16:15 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/12/12 14:51:47 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
-
-void	ft_env_add_back(t_env **envs, t_env *new)
-{
-	t_env	*temp;
-
-	temp = *envs;
-	if (!temp)
-	{
-		temp = new;
-		return ;
-	}
-	while (temp->next != NULL)
-		temp = temp->next;
-	if (temp->next == NULL)
-		temp->next = new;
-}
 
 void	ft_env_add_front(t_env **envs, t_env *new)
 {
@@ -37,7 +20,7 @@ void	ft_env_add_front(t_env **envs, t_env *new)
 	*envs = new;
 }
 
-t_env *ft_new_node(char *key, char *value, int visible)
+t_env	*ft_new_node(char *key, char *value, int visible)
 {
 	t_env	*node;
 
