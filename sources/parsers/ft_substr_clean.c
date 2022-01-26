fdb9b7df72b2a14d67443e40245af49c9c2dee7b
Lucas Yuri <lucasyuri56@hotmail.com>
2022-01-26 18:59:43 -0300

    fix: env leak and compare

--- sources/parsers/ft_substr_clean.c
+++ sources/parsers/ft_substr_clean.c
@@ -6,7 +6,7 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 21:30:05 by lyuri-go          #+#    #+#             */
-/*   Updated: 2022/01/25 17:08:40 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2022/01/26 18:53:09 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ static int	ft_includ_env(int *line_ref, char *line, int *arg_ref, char *arg)
 	env_len = 0;
 	env_name = ft_get_env_name(&line_len, line_ref, line);
 	env_var = ft_get_env(env_name);
-	free(env_name);
 	i = -1;
 	if (env_var)
 		while (env_var[++i] && ++env_len)
@@ -46,6 +45,9 @@ static int	ft_includ_env(int *line_ref, char *line, int *arg_ref, char *arg)
 	else if (++env_len)
 		arg[(*arg_ref)++] = ' ';
 	(*line_ref)--;
+	if (env_name[0] == '?' && !env_name[1])
+		free(env_var);
+	free(env_name);
 	return (env_len - line_len);
 }
 
--- sources/utils/ft_get_env.c
+++ sources/utils/ft_get_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_get_env.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 20:40:25 by lyuri-go          #+#    #+#             */
-/*   Updated: 2022/01/25 21:53:29 by elima-me         ###   ########.fr       */
+/*   Updated: 2022/01/26 18:58:41 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 char	*ft_get_env(char *env)
 {
 	t_env	*envs;
+	size_t	env_len;
 
 	envs = (mini_s()->envs);
+	env_len = ft_strlen(env);
 	while (envs->next != NULL)
 	{
-		if (!ft_strncmp(env, envs->key, ft_strlen(env)))
+		if (envs->visible == 0)
+			return (NULL);
+		if (!ft_strncmp(env, envs->key, env_len)
+			&& ft_strlen(envs->key) == env_len)
 			return (envs->value);
-		if (!ft_strncmp(env, "?", strlen("?")))
+		if (!ft_strncmp(env, "?", 1) && 1 == env_len)
 			return (ft_itoa(mini_s()->last_exec_code));
 		envs = envs->next;
 	}
