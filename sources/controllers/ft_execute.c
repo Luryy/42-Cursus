8be208cffe03382afb2e90fd2bb0b207ee46e5ea
Lucas Yuri <lucasyuri56@hotmail.com>
2022-01-26 18:32:11 -0300

    fix: built-ins compares and status codes

--- sources/controllers/ft_execute.c
+++ sources/controllers/ft_execute.c
@@ -6,7 +6,7 @@
 /*   By: lyuri-go <lyuri-go@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 19:41:12 by lyuri-go          #+#    #+#             */
-/*   Updated: 2022/01/26 10:22:21 by lyuri-go         ###   ########.fr       */
+/*   Updated: 2022/01/26 17:26:21 by lyuri-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 
 void	ft_execute_cmd(t_exec *exec, int shouldfork)
 {
-	if (!ft_strncmp(exec->cmd, "echo", 4))
+	if (!ft_strncmp(exec->cmd, "echo", 4) && ft_strlen(exec->cmd) == 4)
 		ft_echo(exec->args);
-	else if (!ft_strncmp(exec->cmd, "cd", 2))
+	else if (!ft_strncmp(exec->cmd, "cd", 2) && ft_strlen(exec->cmd) == 2)
 		ft_cd(exec->args);
-	else if (!ft_strncmp(exec->cmd, "pwd", 3))
+	else if (!ft_strncmp(exec->cmd, "pwd", 3) && ft_strlen(exec->cmd) == 3)
 		ft_pwd();
-	else if (!ft_strncmp(exec->cmd, "export", 6))
+	else if (!ft_strncmp(exec->cmd, "export", 6) && ft_strlen(exec->cmd) == 6)
 		ft_export(exec->args);
-	else if (!ft_strncmp(exec->cmd, "unset", 5))
+	else if (!ft_strncmp(exec->cmd, "unset", 5) && ft_strlen(exec->cmd) == 5)
 		ft_unset(exec->args);
-	else if (!ft_strncmp(exec->cmd, "env", 3))
+	else if (!ft_strncmp(exec->cmd, "env", 3) && ft_strlen(exec->cmd) == 3)
 		ft_env();
-	else if (!ft_strncmp(exec->cmd, "exit", 4))
+	else if (!ft_strncmp(exec->cmd, "exit", 4) && ft_strlen(exec->cmd) == 4)
 		ft_exit(exec);
-	else
+	else if (exec->cmd[0])
+	{
 		ft_exec_bin(exec->cmd, exec->args, shouldfork);
+		if (!shouldfork)
+			exit(EXIT_SUCCESS);
+		return ;
+	}
 	if (!shouldfork)
 		exit(EXIT_SUCCESS);
 	mini_s()->last_exec_code = 0;
